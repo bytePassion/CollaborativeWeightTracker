d385a0c302be1ffdda2d9358a95cd52058692cd2
Alexander Horn <alexomanie@gmail.com>
2018-10-18 21:01:58 +0200

    cleanup and rename author to user

--- src/graphql.schema.ts
+++ src/graphql.schema.ts
@@ -3,6 +3,11 @@ export enum Gender {
     female = "female"
 }
 
+export enum WeightUnit {
+    kg = "kg",
+    lbs = "lbs"
+}
+
 export class WeightEntryInput {
     weight?: number;
     timestamp?: number;
@@ -13,14 +18,8 @@ export class Author {
     gender?: Gender;
 }
 
-export class LoginInformation {
-    token?: string;
-}
-
 export abstract class IMutation {
     abstract createWeightEntry(createWeightEntryInput?: WeightEntryInput): WeightEntry | Promise<WeightEntry>;
-
-    abstract login(email: string): LoginInformation | Promise<LoginInformation>;
 }
 
 export abstract class IQuery {
@@ -28,7 +27,17 @@ export abstract class IQuery {
 
     abstract weightEntry(id: string): WeightEntry | Promise<WeightEntry>;
 
+    abstract user(id: number): User | Promise<User>;
+
+    abstract getWeightEntriesByUser(): WeightEntry[] | Promise<WeightEntry[]>;
+
     abstract temp__(): boolean | Promise<boolean>;
+}
+
+export class User {
+    id: number;
+    name?: string;
+    gender?: Gender;
 }
 
 export class WeightEntry {
@@ -36,4 +45,6 @@ export class WeightEntry {
     weight?: number;
     timestamp?: number;
     author?: Author;
+    unit?: WeightUnit;
+    user?: User;
 }
--- src/weight/weight-entries.service.ts
+++ src/weight/weight-entries.service.ts
@@ -1,9 +1,19 @@
 import {Injectable} from "@nestjs/common";
-import {Gender, WeightEntry} from "../graphql.schema";
+import {Gender, WeightEntry, WeightUnit} from "../graphql.schema";
 
 @Injectable()
 export class WeightEntriesService {
-    private readonly weightEntries: WeightEntry[] = [{id: 1, weight: 112.5, timestamp: 1234, author: {name: 'tester', gender: Gender.male}}];
+    private readonly weightEntries: WeightEntry[] = [{
+        id: 1,
+        weight: 112.5,
+        unit: WeightUnit.kg,
+        timestamp: 1234,
+        user: {
+            id: 1,
+            name: 'tester',
+            gender: Gender.male
+        }
+    }];
 
     create(entry: WeightEntry): WeightEntry {
         this.weightEntries.push(entry);
