fba5fea718d7ea43c87a8deec0aafb414f9b2b9a
Alexander Horn <alexomanie@gmail.com>
2018-10-22 07:10:50 +0200

    import mongoose package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import {Module} from '@nestjs/common';
 import {GraphQLModule} from '@nestjs/graphql';
 import {WeightEntriesModule} from "./weight/weight-entries.module";
+import { MongooseModule } from '@nestjs/mongoose';
 import {join} from "path";
 
 @Module({
     imports: [
         WeightEntriesModule,
+        MongooseModule.forRoot('mongodb://localhost/nest'),
         GraphQLModule.forRoot({
             typePaths: ['./**/*.graphql'],
             context: ({req}) => ({req}),
